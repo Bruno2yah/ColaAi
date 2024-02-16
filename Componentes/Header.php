bb44d493e79a6e780bd4918cbd3d956687edac42
Bruno2yah <brunogeanini04@gmail.com>
2024-02-16 11:00:13 -0300

    Site adm home

---
+++ Componentes/Header.php
@@ -0,0 +1,24 @@
+<Header class = "w-100" style = "height: 10vh; background-color: #fff4c6">
+    <div class = "row w-100 h-100">
+        <div class = "col-2 h-100 justify-content-center align-items-center d-flex pt-1">
+            <img src="../img/Admin/Logo.png" class = "img-fluid w-75 h-100" style = "" alt="">
+        </div>
+        <div class = "col-8"></div>
+        <div class = "col-2 h-100 justify-content-center align-items-center d-flex">
+            <div class="dropdown text-end">
+                <a href="#" class="text-white d-block link-body-emphasis text-decoration-none "
+                data-bs-toggle="dropdown" aria-expanded="false">
+                <img src="../img/Admin/userPadrao.jpg" alt="mdo" width="50" height="50" class="rounded-circle">
+                <i class="bi bi-chevron-down" style = "color: black;"></i>
+                </a>
+                <ul class="dropdown-menu text-small">
+                <li><a class="dropdown-item" href="#">Perfil</a></li>
+                <li>
+                    <hr class="dropdown-divider">
+                </li>
+                <li><a class="dropdown-item" href="#">Sign out</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</Header>
---
+++ Componentes/menu.php
@@ -0,0 +1,20 @@
+<div class="col-md-2 p-4" style="background-color:#e8fdd8;">
+  <ul class="nav flex-column menu">
+    <li class=" my-1">
+      <a class="" href="#" style="text-decoration: none; color: #a6a6a6;"><i class="bi bi-house"></i>
+        Home</a>
+    </li>
+    <li class=" my-1">
+      <a class="" href="#" style="text-decoration: none; color: #a6a6a6;"><i class="bi bi-person-fill"></i>
+        ADM</a>
+    </li>
+    <li class=" my-1">
+      <a class="" href="#" style="text-decoration: none; color: #a6a6a6;"><i class="bi bi-person-fill"></i>
+        Organização</a>
+    </li>
+    <li class=" my-1">
+      <a class="" href="#" style="text-decoration: none; color: #a6a6a6;"><i class="bi bi-person-fill"></i>
+        Usuário</a>
+    </li>
+  </ul>
+</div>
