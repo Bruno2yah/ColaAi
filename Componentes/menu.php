fd01a8b9bace824f302b97c8e5dc33d88fcf0dd5
Bruno2yah <brunogeanini04@gmail.com>
2024-02-16 11:13:11 -0300

    Links prontos

--- Componentes/menu.php
+++ Componentes/menu.php
@@ -1,19 +1,19 @@
 <div class="col-md-2 p-4" style="background-color:#e8fdd8;">
   <ul class="nav flex-column menu">
     <li class=" my-1">
-      <a class="" href="#" style="text-decoration: none; color: #a6a6a6;"><i class="bi bi-house"></i>
+      <a class="" href="../Home" style="text-decoration: none; color: #a6a6a6;"><i class="bi bi-house"></i>
         Home</a>
     </li>
     <li class=" my-1">
-      <a class="" href="#" style="text-decoration: none; color: #a6a6a6;"><i class="bi bi-person-fill"></i>
-        ADM</a>
+      <a class="" href="../Admin" style="text-decoration: none; color: #a6a6a6;"><i class="bi bi-person-fill"></i>
+        Administrador</a>
     </li>
     <li class=" my-1">
-      <a class="" href="#" style="text-decoration: none; color: #a6a6a6;"><i class="bi bi-person-fill"></i>
+      <a class="" href="../Organizacao" style="text-decoration: none; color: #a6a6a6;"><i class="bi bi-person-fill"></i>
         Organização</a>
     </li>
     <li class=" my-1">
-      <a class="" href="#" style="text-decoration: none; color: #a6a6a6;"><i class="bi bi-person-fill"></i>
+      <a class="" href="../User" style="text-decoration: none; color: #a6a6a6;"><i class="bi bi-person-fill"></i>
         Usuário</a>
     </li>
   </ul>
