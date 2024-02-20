14c8d04af6e073c32ecc3bd1032d5fc01ea3875c
abdera2004 <ruanpereira405@gmail.com>
2024-02-20 17:36:15 -0300

    atualizando navegacao

--- src/routes/navegaçao/Navigation.tsx
+++ src/routes/navegaçao/Navigation.tsx
@@ -4,33 +4,41 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Organizacao from '../pages/Organizacao/index';
 import Usuario from '../pages/Usuario/index';
+import Home from '../pages/Usuario/Home';
 import AcessoSemLogin from '../pages/AcessoSemLogin';
 import ColaAi from '../pages/ColaAi';
-
-import HomeUsuario from '../pages/Usuario/Home';
-import Teatros from '../pages/Usuario/Teatros';
-import Museus from '../pages/Usuario/Museus';
-import Parques from '../pages/Usuario/Parques';
-import Evento from '../pages/Usuario/Evento';
-
 const Stack = createStackNavigator();
 
 export default function Navigation() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="ColaAi" component={ColaAi} />
-        <Stack.Screen name="Organizacao" component={Organizacao} />
-        <Stack.Screen name="Usuario" component={Usuario} />
-        <Stack.Screen name="AcessoSemLogin" component={AcessoSemLogin} />
-
-        <Stack.Screen name="HomeUsuario" component={HomeUsuario} />
-        <Stack.Screen name="Teatros" component={Teatros} />
-        <Stack.Screen name="Museus" component={Museus} />
-        <Stack.Screen name="Parques" component={Parques} />
-        <Stack.Screen name="Evento" component={Evento} />
+        <Stack.Screen
+          name="ColaAi"
+          component={ColaAi}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Organizacao"
+          component={Organizacao}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Usuario"
+          component={Usuario}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="AcessoSemLogin"
+          component={AcessoSemLogin}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
