75f0cf525ff9cee8f8b17a00499c9f4c8e8ad7eb
Your Namecls <you@example.com>
2024-03-13 23:19:17 -0300

    configsPerfil-v1.0

--- src/routes/pages/Usuario/Perfil/index.jsx
+++ src/routes/pages/Usuario/Perfil/index.jsx
@@ -5,10 +5,40 @@ import { useNavigation } from '@react-navigation/native';
 export default function Perfil() {
   const navigation = useNavigation();
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-
+  const [modalDadosVisu, setModalDadosVisu] = useState('false');
+  const [modalSeguindoVisu, setSeguindoVisu] = useState('false');
+  const [modalNotificacoesVisu, setModalNotificacoesVisu] = useState('false');
+  const [modalRegistroInteresseVisu, setModalRegistroInteresseVisu] = useState('false');
+  const [modalFeedbackVisu, setModalFeedbackVisu] = useState('false');
+  const [modalContatoVisu, setModalContatoVisu] = useState('false');
+  const [modalSobreVisu, setModalSobreVisu] = useState('false');
   const toggleDrawer = () => {
     setIsDrawerOpen(!isDrawerOpen);
   };
+
+
+  function dadosPessoaisModal() {
+    setModalDadosVisu('true')
+  }
+  function seguindoModal() {
+    setSeguindoVisu('true')
+  }
+  function notoficacoesModal() {
+    setModalNotificacoesVisu('true')
+  }
+  function registroInteresseModal() {
+    setModalRegistroInteresseVisu('true')
+  }
+  function feedbackModal() {
+    setModalFeedbackVisu('true')
+  }
+  function contatoModal() {
+    setModalContatoVisu('true')
+  }
+  function sobreModal() {
+    setModalSobreVisu('true')
+  }
+
 
   return (
     <ScrollView style={styles.container}>
@@ -33,6 +63,13 @@ export default function Perfil() {
       </View>
       <View style={styles.nomePerfil}>
         <Text style={styles.textUser}>Usuário</Text>
+        <Text style={styles.textUser}>{modalDadosVisu}</Text>
+        <Text style={styles.textUser}>{modalSeguindoVisu}</Text>
+        <Text style={styles.textUser}>{modalNotificacoesVisu}</Text>
+        <Text style={styles.textUser}>{modalRegistroInteresseVisu}</Text>
+        <Text style={styles.textUser}>{modalFeedbackVisu}</Text>
+        <Text style={styles.textUser}>{modalContatoVisu}</Text>
+        <Text style={styles.textUser}>{modalSobreVisu}</Text>
       </View>
       <View style={styles.botaoPerfil}></View>
 
@@ -41,18 +78,106 @@ export default function Perfil() {
         <View style={styles.drawerContainer}>
           {/* Conteúdo do menu hamburguer aqui */}
           <View><Text>Conta</Text></View>
-          <View style={styles.dadoPessoal}>
-            <View>
-              <Image
-                style={{ width: 20, height: 20 }}
-                source={require('../../../../../assets/img/User/profile-user.png')}
-              />
-            </View>
-            <View>
-              <Text>Dados Pessoais</Text></View>
+            <Pressable onPress={()=>dadosPessoaisModal()}>
+              <View style={styles.dadoPessoal}>
+                <View>
+                  <Image
+                    style={{ width: 20, height: 20 }}
+                    source={require('../../../../../assets/img/User/profile-user.png')}
+                  />
+                </View>
+                
+                <View>
+                    <Text>Dados Pessoais</Text>
+                </View>
+              </View>
+            </Pressable>
 
-          </View>
+            <Pressable onPress={()=>seguindoModal()}>
+              <View style={styles.dadoPessoal}>
+                <View>
+                  <Image
+                    style={{ width: 20, height: 20 }}
+                    source={require('../../../../../assets/img/User/profile-user.png')}
+                  />
+                </View>
+                
+                <View>
+                  <Text>Seguindo</Text></View>
+              </View>
+            </Pressable>
+
+            <Pressable onPress={()=>notoficacoesModal()}>
+              <View style={styles.dadoPessoal}>
+                <View>
+                  <Image
+                    style={{ width: 20, height: 20 }}
+                    source={require('../../../../../assets/img/User/profile-user.png')}
+                  />
+                </View>
+                
+                <View>
+                  <Text>Notificações</Text></View>
+              </View>
+            </Pressable>
+
+            <Pressable onPress={()=>registroInteresseModal()}>
+              <View style={styles.dadoPessoal}>
+                <View>
+                  <Image
+                    style={{ width: 20, height: 20 }}
+                    source={require('../../../../../assets/img/User/profile-user.png')}
+                  />
+                </View>
+                
+                <View>
+                  <Text>Registro de Interesse</Text></View>
+              </View>
+            </Pressable>
+            <Pressable onPress={()=>feedbackModal()}>
+              <View><Text>Suporte</Text></View>
+              <View style={styles.dadoPessoal}>
+                <View>
+                  <Image
+                    style={{ width: 20, height: 20 }}
+                    source={require('../../../../../assets/img/User/profile-user.png')}
+                  />
+                </View>
+                
+                <View>
+                  <Text>Feedback</Text></View>
+              </View>
+            </Pressable>
+
+            <Pressable onPress={()=>contatoModal()}>
+              <View style={styles.dadoPessoal}>
+                <View>
+                  <Image
+                    style={{ width: 20, height: 20 }}
+                    source={require('../../../../../assets/img/User/profile-user.png')}
+                  />
+                </View>
+                
+                <View>
+                  <Text>Contato</Text></View>
+              </View>
+            </Pressable>
+
+            <Pressable onPress={()=>sobreModal()}>
+              <View style={styles.dadoPessoal}>
+                <View>
+                  <Image
+                    style={{ width: 20, height: 20 }}
+                    source={require('../../../../../assets/img/User/profile-user.png')}
+                  />
+                </View>
+                
+                <View>
+                  <Text>Sobre</Text></View>
+              </View>
+            </Pressable>
         </View>
+        
       )}
     </ScrollView>
   );
