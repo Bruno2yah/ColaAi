520a12aa4f1df17f471db878fbcc36fccbc487ab
Guimart1 <gui.martins.sts@gmail.com>
2024-03-28 17:54:37 -0300

    Atualização Componentes

---
+++ Adm/Componentes/menu.php
@@ -0,0 +1,69 @@
+<div class="col-3 p-0 linha pt-1 menu-pos" style="background-color:#E6F4D6;">
+    <ul class="nav flex-column menu linha ps-2 pt-5">
+      <a class="menuItems w-100 p-1 ps-2" href="../../Admin/Home/index.php" style="text-decoration: none; color: #a6a6a6;">
+        <li class="">
+          <i class="bi bi-house"></i>
+          <span>Home</span>
+        </li>
+      </a>
+      <a class="menuItems w-100 p-1 ps-2" href="../../Admin/UserAdm/index.php" style="text-decoration: none; color: #a6a6a6;">
+        <li class="">
+          <i class="bi bi-person-fill"></i>
+          <span>Administrador</span>
+        </li>
+      </a>
+      <a class="menuItems w-100 p-1 ps-2" href="../User" style="text-decoration: none; color: #a6a6a6;">
+        <li class="">
+          <i class="bi bi-person-fill"></i>
+          <span>Usuário</span>
+        </li>
+      </a>
+      <p class="d-inline-flex menuItems w-100 p-1 ps-2" style="margin-bottom: 0">
+        <a class="w-100" data-bs-toggle="collapse" href="#collapseOrg" role="button" aria-expanded="false" aria-controls="collapseExample" style="text-decoration: none; color: #a6a6a6;">
+          <i class="bi bi-building-fill"></i>
+          <span>Organização</span>
+        </a>
+      </p>
+      <div class="collapse" id="collapseOrg">
+        <li class="menuItems w-100 p-1 ps-4">
+          <a class="" href="../../Admin/Organizacao/index.php" style="text-decoration: none; color: #a6a6a6;">
+            <i class="bi bi-person-fill"></i>
+            <span>Usuário</span>
+          </a>
+        </li>
+        <li class="menuItems w-100 p-1 ps-4">
+          <a class="" href="../../Admin/Avaliacao/index.php" style="text-decoration: none; color: #a6a6a6;">
+            <i class="bi bi-check-circle-fill"></i>
+            <span>Avaliações</span>
+          </a>
+        </li>
+  
+      </div>
+      <p class="d-inline-flex menuItems w-100 p-1 ps-2" style="margin-bottom: 0">
+        <a class="w-100" data-bs-toggle="collapse" href="#collapseEvento" role="button" aria-expanded="false" aria-controls="collapseExample" style="text-decoration: none; color: #a6a6a6;">
+          <i class="bi bi-calendar-event"></i>
+          <span>Eventos</span>
+        </a>
+      </p>
+      <div class="collapse" id="collapseEvento">
+        <li class="menuItems w-100 p-1 ps-4">
+          <a class="" href="../User" style="text-decoration: none; color: #a6a6a6;">
+            <i class="bi bi-alarm-fill"></i>
+            <span>Horários</span>
+          </a>
+        </li>
+        <li class="menuItems w-100 p-1 ps-4">
+          <a class="" href="../User" style="text-decoration: none; color: #a6a6a6;">
+            <i class="bi bi-calendar-event"></i>
+            <span>Locais</span>
+          </a>
+        </li>
+      </div>
+      <a class="menuItems w-100 p-1 ps-2" href="../Admin" style="text-decoration: none; color: #a6a6a6;">
+        <li class="">
+          <i class="bi bi-sign-stop-fill"></i>
+          <span>Denuncias</span>
+        </li>
+      </a>
+    </ul>
+  </div>
