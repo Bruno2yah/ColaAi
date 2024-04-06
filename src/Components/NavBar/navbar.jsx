267f33b3f7d6c666f4814c3633fd881f440c6dc0
Otávio <pizza131@outlook.com>
2024-04-06 14:31:26 -0300

    Navbar finalizada

--- src/Components/NavBar/navbar.jsx
+++ src/Components/NavBar/navbar.jsx
@@ -1,55 +1,107 @@
-import {View, Image, StyleSheet, Pressable } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-export default function navBar() {
+import React, { useState, useEffect } from 'react';
+import { View, Image, StyleSheet, Pressable } from 'react-native';
+import { useNavigation, useRoute, useFocusEffect  } from '@react-navigation/native';
+
+export default function NavBar() {
     const navigation = useNavigation();
-  return (
-    <View style={styles.container}>
-        <View style={styles.buttonsArea}>
-            <View style={styles.buttons}>
-                <View style={{ justifyContent: 'center', alignItems: 'center',}}>
-                    <Pressable style={styles.buttonNav} onPress={() => navigation.navigate('HomeUsuario')}>
+    const route = useRoute();
+    const selectedBtn = route.params?.selected || 0;
+
+    const [homeColor, setHomeColor] = useState('#FFD932');
+    const [progColor, setProgColor] = useState('#A6A6A6');
+    const [mapColor, setMapColor] = useState('#A6A6A6');
+    const [notifColor, setNotifColor] = useState('#A6A6A6');
+    const [perfilColor, setPerfilColor] = useState('#A6A6A6');
+
+    useEffect(() => {
+        setColor(selectedBtn);
+    }, [selectedBtn]);
+
+    useFocusEffect(() => {
+        setColor(selectedBtn);
+    });
+
+    function setColor(value) {
+        if(value == 0)
+        {
+            setHomeColor('#FFD932');
+            setProgColor('#A6A6A6');
+            setMapColor('#A6A6A6');
+            setNotifColor('#A6A6A6');
+            setPerfilColor('#A6A6A6');
+        }
+        else if(value == 1)
+        {
+            setProgColor('#FFD932');
+            setHomeColor('#A6A6A6');
+            setMapColor('#A6A6A6');
+            setNotifColor('#A6A6A6');
+            setPerfilColor('#A6A6A6');
+        }
+        else if(value == 2)
+        {
+            setMapColor('#FFD932');
+            setProgColor('#A6A6A6');
+            setHomeColor('#A6A6A6');
+            setNotifColor('#A6A6A6');
+            setPerfilColor('#A6A6A6');
+        }
+        else if(value == 3)
+        {
+            setNotifColor('#FFD932');
+            setProgColor('#A6A6A6');
+            setHomeColor('#A6A6A6');
+            setMapColor('#A6A6A6');
+            setPerfilColor('#A6A6A6');
+        }
+        else if(value == 4)
+        {
+            setPerfilColor('#FFD932');
+            setProgColor('#A6A6A6');
+            setHomeColor('#A6A6A6');
+            setMapColor('#A6A6A6');
+            setNotifColor('#A6A6A6');
+        }
+    }
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.buttonsArea}>
+                <View style={styles.buttons}>
+                    <Pressable style={styles.buttonNav} onPress={() => { setColor(0); navigation.navigate('HomeUsuario', { selected: 0 }) }}>
                         <Image
-                            style={{width: 61, height: 61, tintColor: '#FFD932'}}
+                            style={{ width: 61, height: 61, tintColor: homeColor }}
                             source={require('../../../assets/img/NavBar/home.png')}
                         />
                     </Pressable>
-                </View>
-                <View style={{ justifyContent: 'center', alignItems: 'center',}}>
-                    <Pressable style={styles.buttonNav} onPress={() => navigation.navigate('ProgramacaoCultural')}>
+                    <Pressable style={styles.buttonNav} onPress={() => { setColor(1); navigation.navigate('ProgramacaoCultural', { selected: 1 }) }}>
                         <Image
-                            style={{width: 59, height: 59, tintColor: '#A6A6A6'}}
+                            style={{ width: 59, height: 59, tintColor: progColor }}
                             source={require('../../../assets/img/NavBar/ingressos.png')}
                         />
                     </Pressable>
-                </View>
-                <View style={{ justifyContent: 'center', alignItems: 'center',}}>
-                    <Pressable style={styles.buttonNav} >
+                    <Pressable style={styles.buttonNav} onPress={() => { setColor(2) }}>
                         <Image
-                            style={{width: 61, height: 61, tintColor: '#A6A6A6'}}
+                            style={{ width: 61, height: 61, tintColor: mapColor }}
                             source={require('../../../assets/img/NavBar/local.png')}
                         />
                     </Pressable>
-                </View>
-                <View style={{ justifyContent: 'center', alignItems: 'center',}}>
-                    <Pressable style={styles.buttonNav} >
+                    <Pressable style={styles.buttonNav} onPress={() => { setColor(3) }}>
                         <Image
-                            style={{width: 61, height: 61, tintColor: '#A6A6A6'}}
+                            style={{ width: 61, height: 61, tintColor: notifColor }}
                             source={require('../../../assets/img/NavBar/notif.png')}
                         />
                     </Pressable>
-                </View>
-                <View style={{ justifyContent: 'center', alignItems: 'center',}} >
-                    <Pressable style={styles.buttonNav}  onPress={() => navigation.navigate('Perfil')}>
+                    <Pressable style={styles.buttonNav} onPress={() => { setColor(4); navigation.navigate('Perfil', { selected: 4 }) }}>
                         <Image
-                            style={{width: 61, height: 61, tintColor: '#A6A6A6'}}
+                            style={{ width: 61, height: 61, tintColor: perfilColor }}
                             source={require('../../../assets/img/NavBar/perfil.png')}
                         />
                     </Pressable>
                 </View>
             </View>
         </View>
-    </View>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
@@ -61,23 +113,25 @@ const styles = StyleSheet.create({
         bottom: 0,
         justifyContent: 'center',
         alignItems: 'center',
-        alignSelf : 'flex-end',
+        alignSelf: 'flex-end',
     },
-
     buttonsArea: {
-        flexDirection:'row',
+        flexDirection: 'row',
         width: '90%',
         alignSelf: 'center',
         position: 'relative',
         justifyContent: 'center',
         alignItems: 'center',
     },
-
     buttons: {
         flex: 1,
-        flexDirection:'row',
-        justifyContent:'space-between',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
         alignItems: 'center',
         alignSelf: 'center',
+    },
+    buttonNav: {
+        justifyContent: 'center',
+        alignItems: 'center',
     },
 })
